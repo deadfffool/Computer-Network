8178b4498d376f9e1cc3ef25aa1228cc05aeed2a
deadfffool <deadfffool@gmail.com>
2023-10-13 10:39:48 +0800

    lab2 origin

---
+++ lab2/StopWait/GBN/CMakeFiles/stop_wait.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/miles/Computer-Network/lab2/StopWait/GBN/StopWait.cpp" "CMakeFiles/stop_wait.dir/StopWait.cpp.o" "gcc" "CMakeFiles/stop_wait.dir/StopWait.cpp.o.d"
+  "/home/miles/Computer-Network/lab2/StopWait/GBN/StopWaitRdtReceiver.cpp" "CMakeFiles/stop_wait.dir/StopWaitRdtReceiver.cpp.o" "gcc" "CMakeFiles/stop_wait.dir/StopWaitRdtReceiver.cpp.o.d"
+  "/home/miles/Computer-Network/lab2/StopWait/GBN/StopWaitRdtSender.cpp" "CMakeFiles/stop_wait.dir/StopWaitRdtSender.cpp.o" "gcc" "CMakeFiles/stop_wait.dir/StopWaitRdtSender.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab2/StopWait/GBN/CMakeFiles/stop_wait.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stop_wait.dir/StopWait.cpp.o"
+  "CMakeFiles/stop_wait.dir/StopWait.cpp.o.d"
+  "CMakeFiles/stop_wait.dir/StopWaitRdtReceiver.cpp.o"
+  "CMakeFiles/stop_wait.dir/StopWaitRdtReceiver.cpp.o.d"
+  "CMakeFiles/stop_wait.dir/StopWaitRdtSender.cpp.o"
+  "CMakeFiles/stop_wait.dir/StopWaitRdtSender.cpp.o.d"
+  "stop_wait"
+  "stop_wait.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stop_wait.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
